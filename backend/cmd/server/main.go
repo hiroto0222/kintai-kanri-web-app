53150a71351e943b3e2617b7cbf6eb4631b1e330
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-05-05 21:13:30 +0900

    refactor: create Server interface

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -8,16 +8,11 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/hiroto0222/kintai-kanri-web-app/config"
-	dbconn "github.com/hiroto0222/kintai-kanri-web-app/db/sqlc"
+	db "github.com/hiroto0222/kintai-kanri-web-app/db/sqlc"
 	_ "github.com/lib/pq" // lib/pqパッケージは直接は使わないが、sql.Open()を呼び出すときに必要
 )
 
-var (
-	server *gin.Engine
-	db     *dbconn.Queries
-)
-
-func init() {
+func main() {
 	// load config
 	config, err := config.LoadConfig(".")
 	if err != nil {
@@ -29,25 +24,47 @@ func init() {
 	if err != nil {
 		log.Fatalf("could not connect to db, %v", err)
 	}
-	db = dbconn.New(conn)
 
+	// create db store
+	store := db.NewStore(conn)
 	fmt.Println("DB connected successfully...")
 
-	server = gin.Default()
+	// start server
+	server := newServer(config, store)
+	log.Fatal(server.start())
 }
 
-func main() {
-	// load config
-	config, err := config.LoadConfig(".")
-	if err != nil {
-		log.Fatalf("could not load config, %v", err)
-	}
+// TODO: Refactor
+// 全てのHTTPリクエストを処理するHTTP APIサーバ
+type Server struct {
+	config config.Config
+	store  db.Store    // dbを持つために構造体にする
+	router *gin.Engine // 各APIリクエストを正しいハンドラに送信して処理する
+}
 
-	// setup router
-	router := server.Group("/api")
-	router.GET("/healthchecker", func(ctx *gin.Context) {
+// newServer creates a new HTTP server
+func newServer(config config.Config, store db.Store) *Server {
+	server := &Server{
+		config: config,
+		store:  store,
+	}
+	server.setupRouter()
+
+	return server
+}
+
+// setupRouter sets up the HTTP router for all api endpoints
+func (server *Server) setupRouter() {
+	router := gin.Default()
+	apiRoutes := router.Group("/api")
+	apiRoutes.GET("/healthchecker", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": "Server is running!"})
 	})
 
-	log.Fatal(server.Run(":" + config.Port))
+	server.router = router
+}
+
+// start runs the HTTP server on config port
+func (server *Server) start() error {
+	return server.router.Run(":" + server.config.Port)
 }
