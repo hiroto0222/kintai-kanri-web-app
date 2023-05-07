a04765ca06b2db87bc010c2dc87a0baee8324a13
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-05-07 17:32:13 +0900

    feat: add mui, react-router-dom@6

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
+const App = () => {
   return (
-    <>
-      <div>Hello World</div>
-    </>
+    <div>
+      <h1>Hello World</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   );
-}
+};
 
 export default App;
