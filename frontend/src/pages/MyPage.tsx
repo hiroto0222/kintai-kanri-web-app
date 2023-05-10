2c2881febed7188363797b63c062b32edbee421d
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-05-10 19:51:05 +0900

    feat: display list of clockins

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -3,15 +3,20 @@ import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { authContext } from "../context/auth";
 import useAuthApi from "../hooks/api/useAuthApi";
-import useClockApi from "../hooks/api/useClockApi";
+import useClockInsClockOutsApi from "../hooks/api/useClockInsClockOutsApi";
 import useEmployees from "../hooks/api/useEmployeesApi";
+import useListClockIns from "../hooks/api/useListClockIns";
+import usePrivateAxios from "../hooks/usePrivateAxios";
 
 const MyPage = () => {
-  const { getEmployeeById } = useEmployees();
-  const { clockIn } = useClockApi();
-  const { logout } = useAuthApi();
+  const privateApi = usePrivateAxios();
+  const { getEmployeeById } = useEmployees(privateApi);
+  const { clockIn, clockOut } = useClockInsClockOutsApi(privateApi);
+  const { logout } = useAuthApi(privateApi);
   const { authState } = useContext(authContext);
   const navigate = useNavigate();
+
+  const { clockIns } = useListClockIns(privateApi);
 
   return (
     <div>
@@ -57,6 +62,34 @@ const MyPage = () => {
       >
         Clock In
       </Button>
+      <Button
+        variant="contained"
+        onClick={() => {
+          if (authState.user) clockOut(authState.user?.id);
+        }}
+      >
+        Clock Out
+      </Button>
+      <Card>
+        <CardContent>
+          <div>
+            {clockIns &&
+              clockIns.map((clockIn) => (
+                <ul key={clockIn.clock_in_id}>
+                  <li>clock in id: {clockIn.clock_in_id}</li>
+                  <li>
+                    clock in time: {new Date(clockIn.clock_in_time).toString()}
+                  </li>
+                  <li>clock out id: {clockIn.clock_out_id.Int32}</li>
+                  <li>
+                    clock out time:{" "}
+                    {new Date(clockIn.clock_out_time.Time).toString()}
+                  </li>
+                </ul>
+              ))}
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 };
