7d6e070b754fb213386a2036b8669d08ca73e218
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-05-11 02:15:58 +0900

    feat: remove max page_size binding for list employees

--- backend/controllers/employee.controller.go
+++ backend/controllers/employee.controller.go
@@ -86,7 +86,7 @@ func (c *EmployeeController) GetEmployee(ctx *gin.Context) {
 
 type listEmployeesRequest struct {
 	PageID   int32 `form:"page_id" binding:"required,min=1"`
-	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+	PageSize int32 `form:"page_size" binding:"required,min=5"`
 }
 
 func (c *EmployeeController) ListEmployees(ctx *gin.Context) {
