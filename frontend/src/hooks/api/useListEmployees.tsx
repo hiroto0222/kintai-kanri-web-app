f91e5a37ece9fdcb30cd6f7ec00c80c29c0d8a25
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-05-11 02:22:17 +0900

    feat: add ListEmployees hook

---
+++ frontend/src/hooks/api/useListEmployees.tsx
@@ -0,0 +1,39 @@
+import { AxiosInstance, AxiosResponse } from "axios";
+import { useContext, useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import { authContext } from "../../context/auth";
+import { Employee } from "../../services/employees";
+
+const useListEmployees = (privateApi: AxiosInstance) => {
+  const [loading, setLoading] = useState(true);
+  const [employees, setEmployees] = useState<Employee[]>([]);
+  const { authState } = useContext(authContext);
+
+  useEffect(() => {
+    const listEmployees = async () => {
+      const url = `employees`;
+      try {
+        const response: AxiosResponse<Employee[]> = await privateApi.get(url, {
+          withCredentials: true,
+          params: {
+            page_id: 1,
+            page_size: 20, // TODO: add pagination
+          },
+        });
+        toast.success("successfully retrieved employees!");
+        setLoading(false);
+        setEmployees(response.data);
+        console.log(response.data);
+      } catch (error) {
+        toast.error(`an error occured, ${error}`);
+        setLoading(false);
+        setEmployees([]);
+      }
+    };
+    listEmployees();
+  }, [authState, privateApi]);
+
+  return { employees, loading };
+};
+
+export default useListEmployees;
