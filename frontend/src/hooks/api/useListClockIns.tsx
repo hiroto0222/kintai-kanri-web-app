2754e974ebd3a8de9b12651121a024dd680a8965
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-05-31 16:41:37 +0900

    feat: pass employeeId to useListClockIns hook instead of using authState

--- frontend/src/hooks/api/useListClockIns.tsx
+++ frontend/src/hooks/api/useListClockIns.tsx
@@ -1,35 +1,39 @@
 import { AxiosInstance, AxiosResponse } from "axios";
-import { useCallback, useContext, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
-import { authContext } from "../../context/auth";
 import { ListClockInsResponse } from "../../services/clockins";
 
-const useListClockIns = (privateApi: AxiosInstance) => {
+const useListClockIns = (
+  privateApi: AxiosInstance,
+  employeeId: string | undefined
+) => {
   const [loading, setLoading] = useState(true);
   const [clockIns, setClockIns] = useState<ListClockInsResponse[]>([]);
   const [latestClockIn, setLatestClockIn] = useState<ListClockInsResponse>();
-  const { authState } = useContext(authContext);
 
-  const getClockIns = useCallback(async () => {
-    try {
-      const url = `clockins/${authState.user?.id}`;
-      const response: AxiosResponse<ListClockInsResponse[]> =
-        await privateApi.get(url, { withCredentials: true });
-      setLoading(false);
-      setClockIns(response.data);
-      if (response.data && response.data.length > 0) {
-        setLatestClockIn(response.data[0]);
+  const getClockIns = useCallback(
+    async (employeeId: string | undefined) => {
+      try {
+        const url = `clockins/${employeeId}`;
+        const response: AxiosResponse<ListClockInsResponse[]> =
+          await privateApi.get(url, { withCredentials: true });
+        setLoading(false);
+        setClockIns(response.data);
+        if (response.data && response.data.length > 0) {
+          setLatestClockIn(response.data[0]);
+        }
+      } catch (error) {
+        toast.error(`an error occured, ${error}`);
+        setLoading(false);
+        setClockIns([]);
       }
-    } catch (error) {
-      toast.error(`an error occured, ${error}`);
-      setLoading(false);
-      setClockIns([]);
-    }
-  }, [privateApi, authState]);
+    },
+    [privateApi]
+  );
 
   useEffect(() => {
-    getClockIns();
-  }, [authState, privateApi, getClockIns]);
+    getClockIns(employeeId);
+  }, [privateApi, getClockIns, employeeId]);
 
   return { clockIns, latestClockIn, loading, getClockIns };
 };
