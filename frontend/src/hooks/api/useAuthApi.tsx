33500d838607bacdba8fa0a4e0d09e6f88e10a80
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-05-31 12:52:08 +0900

    feat: add error handling for login

--- frontend/src/hooks/api/useAuthApi.tsx
+++ frontend/src/hooks/api/useAuthApi.tsx
@@ -1,6 +1,7 @@
-import { AxiosInstance, AxiosResponse } from "axios";
+import { AxiosError, AxiosInstance, AxiosResponse } from "axios";
 import { useContext } from "react";
 import { toast } from "react-hot-toast";
+import { useTranslation } from "react-i18next";
 import { RegisterFormProps } from "../../components/auth/RegisterForm";
 import { authContext } from "../../context/auth";
 import { AuthActionEnum } from "../../context/auth/authAction";
@@ -8,25 +9,31 @@ import api from "../../services/api";
 import { UserLoginResponse } from "../../services/auth";
 
 const useAuthApi = (privateApi: AxiosInstance) => {
+  const { t } = useTranslation();
   const { authDispatch } = useContext(authContext);
 
   const login = async (email: string, password: string) => {
     const url = "auth/login";
     try {
-      const response: AxiosResponse<UserLoginResponse> = await api.post(
-        url,
-        {
-          email,
-          password,
-        },
-        { withCredentials: true }
-      );
+      const response: AxiosResponse<UserLoginResponse> = await api.post(url, {
+        email,
+        password,
+      });
 
       authDispatch({
         type: AuthActionEnum.LOG_IN,
         payload: response.data,
       });
     } catch (error) {
+      if (error instanceof AxiosError) {
+        if (error.response?.status === 401) {
+          toast.error(t("errors.invalidUserCredentials"));
+          return;
+        } else if (error.response?.status === 404) {
+          toast.error(t("errors.userNotFound"));
+          return;
+        }
+      }
       toast.error(`an error occured, ${error}`);
     }
   };
